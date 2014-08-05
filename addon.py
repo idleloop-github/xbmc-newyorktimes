a135f844da739587faf45551c21bbf1f38783f0e
idleloop-github <idleloop@yahoo.com>
2014-08-05 20:51:58 +0200

    v 1.4.3
    changed video urls as per website changes

--- addon.py
+++ addon.py
@@ -57,8 +57,12 @@ def update_url_for_rtmp(url, XXL4HIRES):
     For brightcove urls, the playpath is after the '&'.
 
     '''
-    if XXL4HIRES == 'true': url=url.replace('_xl_','_xxl_')
-    ###print "XXL4HIRES = " + XXL4HIRES + " url = " + url
+    # 201408: new renditions (video resolutions) have been added... and FLVURL is anchored to 3g resolution (?)
+    #         maybe in the future renditions[] should be parsed and resolution selected by "encodingRate".
+    if XXL4HIRES == 'false': 
+        url=url.replace('_l_3g_mob.mp4','_xl_bb_mm.mp4')
+    else:
+        url=url.replace('_l_3g_mob.mp4','_xxxl_hb_mm.mp4') 
     if url.startswith('rtmp'):
         return '%s playpath=%s' % (url, url.split('&', 1)[1])
     return url
